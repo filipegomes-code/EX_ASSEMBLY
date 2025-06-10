b276f9934598cd24520d73109288664f926c0a1b
Filipe Gomes <ffgomes2004@gmail.com>
2025-06-10 18:43:46 +0100

    ADD: maneira alternativa de fazer com PROCS

--- Exame2023/24_ex1pratica.asm
+++ Exame2023/24_ex1pratica.asm
@@ -23,7 +23,7 @@ comeco_loop:
 
     cmp al, '('
     je nao_copia
-    
+
     mov frase2[di], al
     inc si 
     inc di
@@ -42,3 +42,73 @@ fim:
 
 cseg ends
 end start
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; FAZER O MSM EXERCICIO COM PROCS ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+.8086
+.model small
+.stack 2048
+
+dseg segment
+    frase1 db 'A UC (unidade curricular) de TAC (Tecnologias e Arquiteturas de Computadores) vai ter uma taxa de aprovação elevada $'
+    frase2 db 52 dup(?)
+dseg ends
+
+cseg segment
+    assume cs:cseg, ds:dseg
+
+start:
+    mov ax, dseg
+    mov ds, ax
+
+    lea si, frase1
+    xor di,di
+
+percorrer_frase:
+    call valida_letra
+    inc si
+    mov al, frase1[si]
+    cmp al, '$'
+    je fim
+    jmp percorrer_frase
+
+valida_letra PROC
+
+    mov al, frase1[si]
+    cmp al, '('
+    je ignora
+    
+    mov frase2[di], al
+    inc di
+    ret
+
+    ignora: 
+        call ignora_tudo
+        ret
+
+valida_letra ENDP
+
+ignora_tudo PROC
+    inc si
+
+    nao_copia:
+        mov al, frase1[si]
+        cmp al, ')'
+        jne nao_copia
+        inc si
+        ret
+ignora_tudo ENDP
+
+fim:
+    mov ah, 4Ch
+    int 21h
+
+cseg ends
+end start
+
+
+
+
+
+
